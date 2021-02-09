c9beca46b4a98c168b21576393b7eb057b21a1aa
Ziko <Ziko@gmail.com>
2021-02-09 17:01:29 +0100

    add table justifier in database and resolve some problem

--- ThaLastOneAbsence/ThaLastOneAbsence/Connect.cs
+++ ThaLastOneAbsence/ThaLastOneAbsence/Connect.cs
@@ -22,7 +22,7 @@ namespace ThaLastOneAbsence
         {
             if (con.State == ConnectionState.Closed || con.State == ConnectionState.Broken)
             {
-                con.ConnectionString = "Data Source=DESKTOP-OA38PF8;Initial Catalog=Attendance-Management;Integrated Security=True";
+                con.ConnectionString = @"Data Source=DESKTOP-122P6H8\SQLEXPRESS;Initial Catalog=AttManagement;Integrated Security=True";
                 con.Open();
             }
         }
